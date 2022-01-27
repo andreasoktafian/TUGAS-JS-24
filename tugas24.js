8a675ae35b3df0c4ef2c4a43de0b2012773d7e5c
andreas <andreas.oktafiann@gmail.com>
2022-01-27 14:26:02 +0700

    TUGAS JS 24

---
+++ tugas24.js
@@ -0,0 +1,10 @@
+function panggil() {
+  var array1 = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Sebelumnya :", array1.toString());
+  var asc = array1.sort();
+  console.log("Ascending :", asc.toString());
+  var desc = array1.reverse();
+  console.log("Descending :", desc.toString());
+}
+
+panggil();
